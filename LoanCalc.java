390787c5e4e463d92f89b304e77137e8965ecce6
Eytan Spialter <eytan.spialter@runi.ac.il>
2024-01-02 15:32:14 +0200

    LoanCalc code 01

--- LoanCalc.java
+++ LoanCalc.java
@@ -16,9 +16,10 @@ public class LoanCalc {
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
+
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 		
-		// Computes the periodical payment using brute force search
+		// Computes the periodical payment using brute force search        
 		System.out.print("Periodical payment, using brute force: ");
 		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println();
@@ -39,8 +40,18 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    	iterationCounter = 0;
+    	double payment = loan/n;
+    	while (endBalance(loan, rate, n, payment) > epsilon) {
+    		payment += epsilon;
+    		iterationCounter++;
+    	}
+    	if (Math.abs(endBalance(loan, rate, n, payment)) <= epsilon) {
+    		return payment;
+    	} else {
+    		System.out.println("Failed to find a solution.");
+    		return Double.NaN;
+    	}
     }
     
     /**
@@ -51,8 +62,21 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    	iterationCounter = 0;
+    	double lPay = loan/n, hPay = loan;
+    	double midPay = (lPay + hPay) / 2;
+    	double endBal = endBalance(loan, rate, n, midPay);
+    		while (Math.abs(endBal) > epsilon) {
+    			iterationCounter++;
+    			if (0 < endBal) {
+    				lPay = midPay;
+    			} else {
+    				hPay = midPay;
+    			}
+    			midPay = (lPay + hPay) / 2;
+    			endBal = endBalance(loan, rate, n, midPay);
+    		}
+    	return midPay;
     }
 	
 	/**
@@ -60,7 +84,10 @@ public class LoanCalc {
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
+		for (int i = 1; i <= n; i++) {
+			loan -= payment;
+			loan *= (1 + (rate / 100));
+			}
+		return loan;
 	}
 }
