e5db0b77fda7ec8aa88e373960f68793ce03514d
Eytan Spialter <eytan.spialter@runi.ac.il>
2024-01-03 15:00:46 +0200

    Calendar 01 - start of code

--- Calendar1.java
+++ Calendar1.java
@@ -37,21 +37,67 @@ public class Calendar1 {
 	 // If the month changes, sets the number of days in this month.
 	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
 	 private static void advance() {
-		// Replace this comment with your code
+		if (dayOfWeek < 7) {
+			dayOfWeek++;  	    // advance counter for day in week
+		} else {
+			dayOfWeek = 1;		// end of week - reset day
+		}
+		if (dayOfMonth < nDaysInMonth) {
+			dayOfMonth++;		// advance counter for date of month
+		} else {				// reached end of month
+			dayOfMonth = 1; 	//reset date
+			if (month < 12) {   // advance month
+				month++;
+			} else {       		// end of year - reset month and advance year
+				month = 1; 
+				year++; 
+			}
+			nDaysInMonth = nDaysInMonth(month, year);
+		}
+		
 	 } 
 		 
     // Returns true if the given year is a leap year, false otherwise.
-	private static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+	public static boolean isLeapYear(int year) {
+		boolean isLeapYear = (year % 400 == 0);
+		isLeapYear = isLeapYear || (((year % 4) == 0) && (year % 100 != 0));
+		return isLeapYear;
 	}
 	 
 	// Returns the number of days in the given month and year.
 	// April, June, September, and November have 30 days each.
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
-	private static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+	public static int nDaysInMonth(int month, int year) {
+		int days = 0;
+		switch (month) {
+			case 1:	days = 31;
+					break;
+			case 2: if (isLeapYear(year)) days = 29;
+							else days = 28;
+					break;
+			case 3: days = 31;
+					break;
+			case 4: days = 30;
+					break;
+			case 5: days = 31;
+					break;
+			case 6: days = 30;
+					break;
+			case 7: days = 31;
+					break;
+			case 8: days = 31;
+					break;
+			case 9: days = 30;
+					break;
+			case 10: days = 31;
+					 break;
+			case 11: days = 30;
+					 break;
+			case 12: days = 31;
+					 break;
+		}
+		return days;
+
 	}
 }
