68cdc1fee3f39923d1a4dafb314e35ce37b79102
Eytan Spialter <eytan.spialter@runi.ac.il>
2024-01-04 22:31:47 +0200

    took out unneccessary code

--- Calendar.java
+++ Calendar.java
@@ -17,8 +17,6 @@ public class Calendar {
 		// Advances the date and the day-of-the-week from 1/1/1900 till the wanted year.
 	   	// Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".	
 		// The following variable, used for debugging purposes, counts how many days were advanced so far.
-	    // int debugDaysCounter = 0; 
-		int startingSundaysCounter = 0;
         int requestedYear = Integer.parseInt(args[0]);
         while (year<requestedYear) {
             advance();
@@ -27,19 +25,11 @@ public class Calendar {
 			System.out.print(dayOfMonth + "/" + month + "/" + year);
 			if (dayOfMonth == 1 && dayOfWeek == 1)	 {
 				System.out.print(" Sunday");
-				startingSundaysCounter++;
 			}
 			System.out.println();
 			advance();
-	 		//debugDaysCounter++;
-	 		//// If you want to stop the loop after n days, replace the condition of the
-	 		//// if statement with the condition (debugDaysCounter == n)
-	 		//if (false) { 
-	 		//	break;
-	 		//}
         }
-		// System.out.println("During the 20th century, " + startingSundaysCounter + " Sundays fell on the first day of the month");
-}
+    }
 	
 	 // Advances the date (day, month, year) and the day-of-the-week.
 	 // If the month changes, sets the number of days in this month.
@@ -106,6 +96,6 @@ public class Calendar {
 					 break;
 		}
 		return days;
-
+        
 	}
 }
